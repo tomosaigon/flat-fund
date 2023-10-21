a4b633336e4c887b8619ddbe03b7b37ca9de39f1
Tomo <tomo@crowdeo.com>
2023-10-21 21:19:59 +0700

    feat: TableLand pips_80001_8036

--- packages/nextjs/pages/tableland.tsx
+++ packages/nextjs/pages/tableland.tsx
@@ -22,58 +22,73 @@ function walletClientToSigner(walletClient: WalletClient) {
     return signer
 }
 
-interface TableSchema {
-    id: number;
-    val: string;
+const matcherBobbyTables = async (walletClient: WalletClient) => {
+    // const [signer] = await getSigners();
+    // console.log(signer);
+    debugger
+    // const db = new Database({ signer });
+    // const { data: walletClient } = useWalletClient({ chainId: 80001 });
+    const signer = walletClientToSigner(walletClient)
+    const db = new Database({ signer });
+
+    const prefix = "my_table";
+    // const { meta: create } = await db
+    //   .prepare(`CREATE TABLE ${prefix} (id integer primary key, val text);`)
+    //   .run();
+
+    // // The table's `name` is in the format `{prefix}_{chainId}_{tableId}`
+    // const tableName = create.txn?.name ?? ""; // e.g., my_table_31337_2
+    // await create.txn?.wait();
+
+    const tableName = 'matcher_80001_7922';
+    // const { meta: insert } = await db
+    //   .prepare(`INSERT INTO ${tableName} (id, val) VALUES (?, ?);`)
+    //   .bind(0, "Bobby Tables")
+    //   .run();
+    const { meta: insert } = await db
+        .prepare(`INSERT INTO ${tableName} (token, xmtp) VALUES (?, ?);`)
+        .bind("Bobby", "Tables")
+        .run();
+
+    console.log(insert);
+    // Wait for transaction finality
+    await insert.txn?.wait();
+    console.log(insert);
+
+    const { results } = await db.prepare(`SELECT * FROM ${tableName};`).all();
+    console.log(results);
+
+}
+const pipsAdd = async (walletClient: WalletClient) => {
+    // const { data: walletClient } = useWalletClient({ chainId: 80001 });
+    const signer = walletClientToSigner(walletClient)
+    const db = new Database({ signer });
+
+    const now = new Date();
+    const hh = String(now.getHours()).padStart(2, '0');
+    const mm = String(now.getMinutes()).padStart(2, '0');
+    const ss = String(now.getSeconds()).padStart(2, '0');
+    const prefix = "pips";
+    const tableName = 'pips_80001_8036';
+    const { meta: insert } = await db
+        // .prepare(`INSERT INTO ${tableName} (id, addr) VALUES (?, ?);`)
+        .prepare(`INSERT INTO ${tableName} (addr) VALUES (?);`)
+        .bind("0x9820E0447e7633d765c4AcD583ef94D0eB" + hh + mm + ss)
+        .run();
+
+    console.log(insert);
+    // Wait for transaction finality
+    await insert.txn?.wait();
+    console.log(insert);
+
+    const { results } = await db.prepare(`SELECT * FROM ${tableName};`).all();
+    console.log(results);
+
 }
 
-const ExampleUI: NextPage = () => {
-    const { data: walletClient, isError, isLoading, status } = useWalletClient({ chainId: 80001 });
-    // const { data: walletClient, isError, isLoading, status } = useWalletClient({  });
-    useEffect(() => {
-        if (isLoading) return;
-        console.log(status);
-        console.log(walletClient);
-        if (walletClient === undefined) return;
-        (async () => {
-            // const [signer] = await getSigners();
-            // console.log(signer);
-            debugger
-            // const db = new Database({ signer });
-            // const { data: walletClient } = useWalletClient({ chainId: 80001 });
-            const signer = walletClientToSigner(walletClient as WalletClient)
-            const db = new Database({ signer });
-
-            const prefix = "my_table";
-            // const { meta: create } = await db
-            //   .prepare(`CREATE TABLE ${prefix} (id integer primary key, val text);`)
-            //   .run();
-
-            // // The table's `name` is in the format `{prefix}_{chainId}_{tableId}`
-            // const tableName = create.txn?.name ?? ""; // e.g., my_table_31337_2
-            // await create.txn?.wait();
-
-            const tableName = 'matcher_80001_7922';
-            // const { meta: insert } = await db
-            //   .prepare(`INSERT INTO ${tableName} (id, val) VALUES (?, ?);`)
-            //   .bind(0, "Bobby Tables")
-            //   .run();
-            const { meta: insert } = await db
-                .prepare(`INSERT INTO ${tableName} (token, xmtp) VALUES (?, ?);`)
-                .bind("Bobby", "Tables")
-                .run();
-
-            console.log(insert);
-            // Wait for transaction finality
-            await insert.txn?.wait();
-            console.log(insert);
-
-            const { results } = await db.prepare(`SELECT * FROM ${tableName};`).all();
-            console.log(results);
-
-        })();
-    }, [walletClient]);
-
+const TableUI: NextPage = () => {
+    // const { data: walletClient, isError, isLoading, status } = useWalletClient({ chainId: 80001 });
+    const { data: walletClient, isError, isLoading, status } = useWalletClient({  });
     return (
         <>
             <MetaHeader
@@ -84,12 +99,19 @@ const ExampleUI: NextPage = () => {
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree&display=swap" rel="stylesheet" />
             </MetaHeader>
-            {/* <div className="grid lg:grid-cols-2 flex-grow" data-theme="TablelandUi">
-                <ContractInteraction />
-                <ContractData />
-            </div> */}
+
+            <div className="grid lg:grid-cols-2 flex-grow" data-theme="TablelandUi">
+                <button
+                    disabled={isLoading || isError}
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    // onClick={() => matcherBobbyTables(walletClient as WalletClient)}
+                    onClick={() => pipsAdd(walletClient as WalletClient)}
+                >
+                    Click Me
+                </button>
+            </div>
         </>
     );
 };
 
-export default ExampleUI;
+export default TableUI;
